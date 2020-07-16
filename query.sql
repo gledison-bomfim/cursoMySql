1a9eb6c6737c3dc6d8ccc00442bddfe5c6702085
gledison-bomfim <gledisonab@gmail.com>
2020-07-16 11:29:06 -0300

    query da aula 11

---
+++ query.sql
@@ -0,0 +1,16 @@
+use cadastro;
+show tables;
+
+select  * from gafanhotos;
+
+select * from cursos 
+order by nome;
+
+select * from cursos 
+order by nome desc;
+
+select * from cursos 
+order by idcurso desc;
+
+select nome, carga, ano from cursos
+order by nome;
