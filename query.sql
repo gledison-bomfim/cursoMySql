5f6db11944b101dd0a6ff90b8839a2fdf69adce9
gledison-bomfim <gledisonab@gmail.com>
2020-07-16 11:53:17 -0300

    querys of movie 11

--- query.sql
+++ query.sql
@@ -1,5 +1,5 @@
 use cadastro;
-show tables;
+
 
 select  * from gafanhotos;
 
@@ -14,3 +14,34 @@ order by idcurso desc;
 
 select nome, carga, ano from cursos
 order by nome;
+
+SELECT * FROM cursos WHERE ano = '2016'
+ORDER BY nome;
+
+SELECT  nome, descricao, carga FROM cursos 
+WHERE ano = '2016'
+ORDER BY nome;
+
+
+SELECT  nome, descricao, ano FROM cursos 
+WHERE ano <= '2016'
+ORDER BY nome;
+
+SELECT  nome, descricao, ano FROM cursos 
+WHERE ano > '2016'
+ORDER BY ano, nome;
+
+SELECT  * FROM cursos 
+WHERE totaulas BETWEEN '20' and '30'
+ORDER BY nome;
+
+SELECT  * FROM cursos 
+WHERE ano in (2010, 2015)
+ORDER BY nome;
+
+SELECT  * FROM cursos 
+WHERE ano not in (2010, 2015)
+ORDER BY nome;
+
+SELECT  * FROM cursos 
+WHERE carga >35 and totaulas < 30;
